f074d86e5e5b440b0bf190bf2c25ab54219c728b
pushpendu ghosh <pushpendu.ghosh@apptware.com>
2024-12-26 13:21:29 +0530

    update wss file

--- dist/main.js
+++ dist/main.js
@@ -2,10 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const platform_ws_1 = require("@nestjs/platform-ws");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
-    console.log('Server started on http://localhost:3000');
+    const port = process.env.PORT || 3000;
+    app.useWebSocketAdapter(new platform_ws_1.WsAdapter(app));
+    await app.listen(port);
+    const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http';
+    const host = process.env.HOST || 'localhost';
+    console.log(`Server started on ${protocol}://${host}:${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Server started on http://localhost:3000');
-}  
+  // Make sure the WebSocket server uses the correct port
+  const port = process.env.PORT || 3000;
+
+  // Enable WebSocket support with a default adapter
+  app.useWebSocketAdapter(new WsAdapter(app));
+  await app.listen(port);
+  const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'; // assuming it's using HTTPS in production
+  const host = process.env.HOST || 'localhost'; // you can use the host from the environment variables or fallback to localhost
+
+  console.log(`Server started on ${protocol}://${host}:${port}`);
+  }
+
 bootstrap();
